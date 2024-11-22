5f3233673282d6acfd738b9ffd60b34ffaf67753
Andrew Murtha <57697374+APMurtha@users.noreply.github.com>
2024-11-22 12:41:50 -0500

    Create CatppuccinHack.js
    first attempt

---
+++ CatppuccinHack.js
@@ -0,0 +1,26 @@
+black        = '#45475A';
+red          = '#F38BA8';
+green        = 'A6E3A1';
+yellow       = '#F9E2AF';
+blue         = '#89B4FA';
+magenta      = '#F5C2E7';
+cyan         = '#94E2D5';
+white        = '#BAC2DE';
+lightBlack   = '#585B70';
+lightRed     = '#F38BA8';
+lightGreen   = '#A6E3A1';
+lightYellow  = '#F9E2AF';
+lightBlue    = '#89B4FA';
+lightMagenta = '#F5C2E7';
+lightCyan    = '#94E2D5';
+lightWhite   = '#A6ADC8';
+
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', '#F5E0DC');
+t.prefs_.set('foreground-color', '#CDD6F4');
+t.prefs_.set('background-color', '#1E1E2E');
